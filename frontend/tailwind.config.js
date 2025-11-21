c405cc739ca7be44c48d6bed4a5a8520f179a5c3
sujan2000 <meena.sujan14@gmail.com>
2025-11-21 15:10:30 +0530

    navbar create

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@ import daisyui from 'daisyui';
 
  /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
+   content: ["./src/**/*.{html,jsx,css}"],
    theme: {
      extend: {},
    },
