882ef4eca86f18c79b560f656e5331834eef0121
sujan2000 <meena.sujan14@gmail.com>
2025-11-23 01:39:59 +0530

    workdone for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// Set the base URL depending on the environment
+const BASE_URL = import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : '/api'; 
+
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: BASE_URL,
 })
 
 export default api;
