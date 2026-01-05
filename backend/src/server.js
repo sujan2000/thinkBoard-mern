103e1100333acf17c1d40fa981a489423045d542
sujan2000 <meena.sujan14@gmail.com>
2026-01-05 21:07:50 +0530

    add credentials

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,8 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(cors({
-        origin: 'http://localhost:5173'
+        origin: 'http://localhost:5173',
+        credentials: true,
     }));
 }
 
